d99f0eadcc1644002b6e2ceadc178a6d05e46b70
scrottty <ollie.steiner@me.com>
2017-05-21 23:20:50 +1200

    Heatmap and video output working
    Need to fix the Hog features across the whole image

---
+++ lessonFunctions.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cv2
+import pickle
+from skimage.feature import hog
+
+
+def convertCSpace(img, color_space='RGB'):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'BGR':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2RGB)
+        elif color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        elif color_space == 'LAB':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+    else: feature_image = np.copy(img)   
+    return feature_image
+
+def bin_spatial(img, size=(32, 32), unravel=True):          
+    # Use cv2.resize().ravel() to create the feature vector
+    if unravel:
+        features = cv2.resize(img, size).ravel()
+    else:
+        features = cv2.resize(img, size)
+    return features
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the RGB channels separately
+    rhist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    ghist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    bhist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Generating bin centers
+    bin_edges = rhist[1]
+    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return rhist, ghist, bhist, bin_centers, hist_features
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
+                                  visualise=True, feature_vector=False)
+        return features, hog_image
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
+                       visualise=False, feature_vector=feature_vec)
+        return features
+
